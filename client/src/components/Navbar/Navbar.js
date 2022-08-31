7869e0406a9fdf685a06c5013da1953c70d29e7f
Matheus Barbosa <mattbarb93@gmail.com>
2022-08-31 14:13:10 -0400

    Added images and font to options

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import NavLogo from "../../assets/shared/desktop/logo.svg";
+import IconCart from "../../assets/shared/desktop/icon-cart.svg";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="navbar-background">
+    <nav className="navbar-background">
       <div className="navbar-options">
-        <div className="navbar-icon">
-          <img href={NavLogo} class="img-fluid" alt="Responsive image" />
+        <div className="d-flex justify-content-center align-items-center">
+          <img src={NavLogo} class="img-fluid" alt="Responsive image" />
         </div>
 
         <ul className="navbar-pages">
@@ -16,11 +17,11 @@ const Navbar = () => {
           <a>Speakers</a>
           <a>Earphones</a>
         </ul>
-        <div className="navbar-cart">
-          <img href="../assets/shared/desktop/icon-cart.svg"></img>
+        <div className="d-flex justify-content-center align-items-center">
+          <img src={IconCart}></img>
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
