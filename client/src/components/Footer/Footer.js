81d93d10d1fa6ba514ff3965d03bc0a1aa18200c
Matheus Barbosa <mattbarb93@gmail.com>
2022-09-19 22:36:01 -0400

    Started the footer, kinda

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,7 +2,27 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer-logo"></div>
+      <ul class="footer-pages">
+        <li href="/">Home</li>
+        <li href="/">Headphones</li>
+        <li href="/">Speakers</li>
+        <li href="/">Earphones</li>
+      </ul>
+      <div className="footer-details">
+        Audiophile is an all in one stop to fulfill your audio needs. We're a
+        small team of music lovers and sound specialists who are devoted to
+        helping you get the most out of personal audio. Come and visit our demo
+        facility - we’re open 7 days a week.
+      </div>
+      <div className="footer-social"></div>
+      <div className="footer-copyright">
+        Copyright 2022. All Rights Reserved
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
