eef588ece0a192b16237de712119fe71d1379366
Matheus Barbosa <mattbarb93@gmail.com>
2022-09-21 13:22:57 -0400

    little change on About page

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className="footer bg-dark">
       <div className="footer-logo"></div>
       <ul class="footer-pages">
         <li href="/">Home</li>
